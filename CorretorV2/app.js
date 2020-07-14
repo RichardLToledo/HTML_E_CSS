499342de5caaa90a2fd3169ef72928c85ccda4c4
RichardLToledo <dragonzira@gmail.com>
2020-07-14 17:41:04 -0300

    Animaçoes

--- CorretorV2/app.js
+++ CorretorV2/app.js
@@ -1,5 +1,7 @@
 var scroll = window.requestAnimationFrame ||
-  function (callback) { window.setTimeout(callback, 1000 / 60) };
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60)
+  };
 
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
@@ -22,12 +24,20 @@ function isElementInViewport(el) {
   }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 0 && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight)
-      && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
     (rect.top >= 0 && rect.bottom <= (window.innerHeight || document
       .documentElement.clientHeight))
   );
 }
+
+//Scroll
+
+document.querySelector(".sub").addEventListener("click", function () {
+  window.scrollTo(0, 3400);
+});
+
+document.querySelector(".sub2").addEventListener("click", function () {
+  window.scrollTo(0, 3400);
+});
